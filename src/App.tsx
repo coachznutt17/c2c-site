8cf9d805541ca1170f3ba5def23d79c7e72d40bd
coachznutt17 <coachznutt17@gmail.com>
2025-10-29 09:03:13 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './contexts/AuthContext';
+// trigger redeploy
 
 // Core components that we know exist
 import AnnouncementBar from './components/AnnouncementBar';
