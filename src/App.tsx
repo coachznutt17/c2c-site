cd4bcc1c638306e29e2f9070fa009ad957c3b610
coachznutt17 <coachznutt17@gmail.com>
2025-10-31 12:21:28 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,4 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import LoginFormSimple from './components/LoginFormSimple';
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Toaster position="top-center" />
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginFormSimple />} />
-        <Route path="*" element={<div style={{ padding: 24 }}>Not found</div>} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div style={{ padding: 24, fontSize: 24 }}>APP ALIVE ✅</div>;
 }
 
