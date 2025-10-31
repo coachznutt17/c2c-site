8d98f716e308890c05119096af34303ed2391475
coachznutt17 <coachznutt17@gmail.com>
2025-10-31 12:05:11 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from './contexts/AuthContext';
-// trigger redeploy
+import LoginFormSimple from './components/LoginFormSimple';
 
-// Core components that we know exist
-import AnnouncementBar from './components/AnnouncementBar';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import TrustBar from './components/TrustBar';
-import HowItWorksTabs from './components/HowItWorksTabs';
-import FeaturedCoaches from './components/FeaturedCoaches';
-import FeaturedResources from './components/FeaturedResources';
-import BrowseResources from './components/BrowseResources';
-import Footer from './components/Footer';
-import SignIn from './routes/SignIn';
-
-
-// (Auth-related components REMOVED here to satisfy Vercel for now)
-// import UserSignupForm from './components/UserSignupForm';
-// import LoginFormSimple from './components/LoginFormSimple';
-// import UserProfilePage from './components/UserProfilePage';
-// import ConfirmEmailPage from './components/ConfirmEmailPage';
-
-// Home page layout
-const HomePage: React.FC = () => {
+export default function App() {
   return (
-    <>
-      <Hero />
-      <TrustBar />
-      <HowItWorksTabs />
-      <FeaturedResources />
-      <FeaturedCoaches />
-    </>
-  );
-};
-
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <Toaster position="top-right" />
-        <div className="min-h-screen bg-gray-50">
-          <AnnouncementBar />
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/browse" element={<BrowseResources />} />
-              <Route path="/login" element={<LoginFormSimple />} />
-
-
-              {/*
-                TEMPORARILY DISABLED ROUTES FOR DEPLOY:
-                <Route path="/signup" element={<UserSignupForm />} />
-                <Route path="/login" element={<LoginFormSimple />} />
-                <Route path="/user-profile" element={<UserProfilePage />} />
-                <Route path="/confirm-email" element={<ConfirmEmailPage />} />
-              */}
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </AuthProvider>
+    <BrowserRouter>
+      <Toaster position="top-center" />
+      <Routes>
+        {/* send root to /login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        {/* login page */}
+        <Route path="/login" element={<LoginFormSimple />} />
+        {/* simple 404 fallback */}
+        <Route path="*" element={<div style={{padding:24}}>Not found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
